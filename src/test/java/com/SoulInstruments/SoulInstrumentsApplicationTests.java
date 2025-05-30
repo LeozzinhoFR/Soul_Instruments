cf03201e5c46484b1322abc259c25976cb86cb1d
leoroncolato <leonardo.p.froncolato@gmail.com>
2025-05-30 19:43:24 -0300

    SoulInstruments V1.0

---
+++ src/test/java/com/SoulInstruments/SoulInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoulInstruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulInstrumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
